a1ddafb631f9c9cde95897b28b935cb3b00c4d9a
G Sriram <sgcuber24@gmail.com>
2020-06-29 12:47:26 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # CrowdRepelAPI
+
+This serves the production build of the REST APIs required to run CrowdRepel
+
+
+## Installation
+
+1.  Install Dependencies
+
+    * [Node.js](https://nodejs.org/en/)
+
+3.  Go to the project's root directory **cd /my/path/to/directory**
+4.  Run **npm install**
+5.  Start using it! **npm start**
+
+## Available end-points
+
+
+### GET /api/v1/count
+
+Gets crowd count in all areas.
+
+### POST /api/v1/count
+
+Creates an entry (be sure you are sending the headers via your library).
+
+**Headers**
+
+Content-Type : application/json
+
+**Request body (raw)**
+
+```
+    {
+      "cctvId":"6",
+      "latitude":12.971879,
+      "longitude":77.595739,
+      "peopleCount": 25
+    }
+```
+
--- server.js
+++ server.js
@@ -2,28 +2,24 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({
-  path: './config.env',
+	path: './config.env'
 });
 const app = require('./index');
-const DB = process.env.DATABASE.replace(
-  '<password>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then((con) => {
-    console.log('DB Connected');
-  });
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	})
+	.then((con) => {
+		console.log('DB Connected');
+	});
 
 const port = process.env.PORT || 3000;
-var cors = require('cors');
-app.use(cors());
+
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+	console.log(`App running on port ${port}...`);
 });
